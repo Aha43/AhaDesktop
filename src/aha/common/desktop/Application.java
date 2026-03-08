1fd856ec1ccdc7526157a0e367959b923a86b79f
Arne Halvorsen <arne.halvorsen@gmail.com>
2026-03-08 12:42:00 +0100

    initial commit

---
+++ src/aha/common/desktop/Application.java
@@ -0,0 +1,8 @@
+package aha.common.desktop;
+
+import java.awt.Frame;
+
+public abstract class Application extends Frame {
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+module Astrogeist {
+	requires java.desktop;
+	requires java.base;
+	requires java.xml;
+	requires java.logging;
+	requires java.net.http;
+}
